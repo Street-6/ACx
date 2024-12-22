57926a597003e40442b52a10c9701efbe707c83b
Street-6 <steelernation9710@outlook.com>
2024-12-22 10:21:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import logging
 import uvicorn 
 
 app = FastAPI()
-bot = TeleBot("")
+bot = TeleBot("7608513610:AAEaq5POwJulje5FG5-hG_uzKkL4JSPcw5w")
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
